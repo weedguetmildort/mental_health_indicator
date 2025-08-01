07e69bf3e85438ca27cc5c98cc418513e6a2e55b
weedguet_mildort <weedguet.mildort@gmail.com>
2025-07-31 23:11:41 -0400

    Tokenization

--- main.py
+++ main.py
@@ -55,59 +55,6 @@ def scrape_reddit():
     except TypeError as e:
         print(f"Error serializing dictionary (contains unsupported types?): {e}")
 
-    """
-    #print(reddit.read_only)
-
-    # for submission in reddit.subreddit("test").hot(limit=10):
-    #     print(submission.title)
-
-    # assume you have a praw.Reddit instance bound to variable `reddit`
-    subreddit = reddit.subreddit("redditdev")
-
-    print(subreddit.display_name)
-    # Output: redditdev
-    print(subreddit.title)
-    # Output: reddit development
-    print(subreddit.description)
-    # Output: a subreddit for discussion of ...
-
-    # assume you have a Subreddit instance bound to variable `subreddit`
-    for submission in subreddit.hot(limit=10):
-        print(submission.title)
-        # Output: the submission's title
-        print(submission.score)
-        # Output: the submission's score
-        print(submission.id)
-        # Output: the submission's ID
-        print(submission.url)
-        # Output: the URL the submission points to or the submission's URL if it's a self post
-
-    # Depression, Mental Health
-    # Keywords:
-    """
-
-
-    """
-    import pandas as pd
-
-    reddit = praw.Reddit(
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET',
-        username='YOUR_USERNAME',
-        password='YOUR_PASSWORD',
-        user_agent='MentalHealthScraperBot/0.1'
-    )
-
-    subreddits = ['depression', 'mentalhealth', 'anxiety']
-    posts = []
-
-    for sub in subreddits:
-        for post in reddit.subreddit(sub).hot(limit=500):  # or .new(limit=500)
-            posts.append({
-                'title': post.title,
-                'text': post.selftext,
-
-    """
 
 def clean_data():
     data = pd.read_csv('mentalhealth.csv')
